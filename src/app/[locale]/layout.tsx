eebdb8e0689353d5d5d7d5aae80783df2ac3f346
HADJAM JuGurta <82827355+1u6urta@users.noreply.github.com>
2025-04-19 11:46:28 +0100

    Add files via upload

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import {routing} from '@/i18n/routing';
+ 
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({locale}));
+}
 
 export const metadata: Metadata = {
   title: "Moontajty",
