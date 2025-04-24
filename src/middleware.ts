0a58b4e2823fbf127ce997586ad0ae1054496801
HADJAM JuGurta <82827355+1u6urta@users.noreply.github.com>
2025-04-24 11:02:38 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
+import intlConfig from '../next-intl.config.js';
  
-export default createMiddleware(routing);
- 
+export default createMiddleware(intlConfig);
+  
 export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
