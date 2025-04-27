294240bca37fb37a4391752d48be00e80b754e6b
HADJAM JuGurta <82827355+1u6urta@users.noreply.github.com>
2025-04-27 21:00:39 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
-import nextIntlConfig from './next-intl.config';
  
-const nextConfig: NextConfig = {
- i18n: {
-    locales: nextIntlConfig.locales,
-    defaultLocale: nextIntlConfig.defaultLocale,
-  }
-};
+const nextConfig: NextConfig = {};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
