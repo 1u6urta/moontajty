be7a2a14f542161e7c0b4eaf641d3a71100cf602
HADJAM JuGurta <82827355+1u6urta@users.noreply.github.com>
2025-04-27 22:22:35 +0100

    Delete i18n.js

--- i18n.js
+++
@@ -1,8 +0,0 @@
-import {getRequestConfig} from 'next-intl/server';
-import {locales} from './next-intl.config';
-
-export default getRequestConfig(async ({locale}) => {
-  return {
-    messages: (await import(`./messages/${locale}.json`)).default
-  };
-});
