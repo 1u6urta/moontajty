b1d6989f0663fab59d34c99e6f14c6301f2c48b5
HADJAM JuGurta <82827355+1u6urta@users.noreply.github.com>
2025-04-27 22:11:23 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n.js');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Add any other config options you need here
+};
 
 export default withNextIntl(nextConfig);
